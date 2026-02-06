b86e2d991cc9ae6c1e00b3e454a12a27c6ba5d6b
wowcosplayg <wowcosplay@gmail.com>
2026-02-06 20:41:11 +0800

    fix(docker): Bypass interactive menu in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ chmod +x /app/main.sh /app/modules/*.sh
 # Run the Main Orchestrator
 # This will configure and start services in background (nohup mode)
 echo "Starting Argosbx Orchestrator..."
-/app/main.sh
+/app/main.sh install
 
 # Keep container alive by tailing logs
 # We wait a bit for logs to be created
